d79544fa6b7552eee0a15d385587d197fd315f3e
Piyush Dravyakar <piyushshdravyakar@VF298.local>
2026-07-05 14:41:03 +0530

    add new

--- e2e_tests/utilities/configpar.py
+++ e2e_tests/utilities/configpar.py
@@ -1,12 +1,28 @@
+# import configparser
+# import os
+#
+# class ConfigReader:
+#     @staticmethod
+#     def read_config(section, key):
+#         config = configparser.ConfigParser()
+#         config_path = r"//config/config.ini"
+#         config.read(config_path)
+#         return config[section][key]
+#
+#
 import configparser
 import os
 
 class ConfigReader:
+
     @staticmethod
     def read_config(section, key):
         config = configparser.ConfigParser()
-        config_path = r"//config/config.ini"
-        config.read(config_path)
-        return config[section][key]
 
+        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        config_path = os.path.join(base_dir, "config", "config.ini")
+
+        config.read(config_path)
+
+        return config[section][key]
 
